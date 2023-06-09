1fe5728a4caf03738a5dc1aa0c631150df03ce21
lijiakang <1772924569@qq.com>
2023-06-09 23:06:13 +0800

    连接数据库

---
+++ 连接数据库/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// 定义一个全局对象db
+var db *sql.DB
+
+// 定义一个初始化数据库的函数
+func initDB() (err error) {
+	// DSN:Data Source Name
+	dsn := "root:ljk15130918589@tcp(127.0.0.1:3306)/haha?charset=utf8mb4&parseTime=True"
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return err
+	}
+	// 尝试与数据库建立连接（校验dsn是否正确）
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	err := initDB() // 调用输出化数据库的函数
+	if err != nil {
+		fmt.Printf("init db failed,err:%v\n", err)
+		return
+	}
+}
